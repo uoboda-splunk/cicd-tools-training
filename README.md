5bec67178e02d16dd1a60093987445e50a4ef949
uoboda-splunk <uoboda@splunk.com>
2021-01-20 23:38:13 +0100

    test with docker

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cicd-tools-training
 Repository created to present different CI/CD tools e.g. CircleCI, Github Actions
+
+CirclecI documentation: https://circleci.com/docs/
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,12 @@
+# import requests
 from app.app import get_info_string
 
 
 def test_info_string():
     assert get_info_string() == "Hello, World!"
+
+
+# def test_with_container():
+#     response = requests.get("http://localhost:8000")
+#     assert response.status_code == 200
+#     assert response.text == "Hello, World!"
